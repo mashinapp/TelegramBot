6803fea4efa0bbed6507c72d10630776012e94ee
mashina_pp <103668647+mashinapp@users.noreply.github.com>
2022-06-30 21:52:12 +0300

    telegrambot

---
+++ Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TelegramBot.Constant
+{
+    public class Constants
+    {
+        public static string adress = "https://localhost:7096/";
+        public static string apikey = "a711291e80a4b45b1d38f84efc0029fbbac8a235c74e11437d7ade39b4757a73";
+    }
+}
+
---
+++ JobSearch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace TelegramBot.Model
+{
+    public class JobSearch
+    {
+        public List<JobsResult> jobs_results { get; set; }
+
+    }
+
+
+    public class JobsResult
+    {
+        public string title { get; set; }
+        public string company_name { get; set; }
+        public string location { get; set; }
+        public string via { get; set; }
+        public string description { get; set; }
+
+    }
+
+}
+
